8d15f98229cf31f6e8a1ac5b600d15e3492765cc
Gregory Fournier <gregoryfournier@Gregorys-MacBook-Pro.local>
2014-11-25 15:48:46 -0500

    remove all files

--- TP2_LOG2810/Transition.cpp
+++
@@ -1,9 +0,0 @@
-#include "Transition.h"
-
-#pragma once
-
-Transition::Transition(Etat initialState, Etat endState, string letters) {
-	initialState_ = initialState;
-	endState_ = endState;
-	letters_ = letters;
-}
